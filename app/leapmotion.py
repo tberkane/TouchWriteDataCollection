f18b832d8da1ca4f2fde251723fbb00552713ab6
Thomas <thomas@berkane.fr>
2022-11-23 10:06:20 +0100

    add doc

--- app/leapmotion.py
+++ app/leapmotion.py
@@ -1,28 +1,31 @@
-import sys
 import json
+import sys
 import time
-from threading import Thread, Timer
-from playsound import playsound
-from resources.LeapSDK.v53_python39 import Leap
 from threading import Thread
+from threading import Timer
 
-_JOINT_POSITIONS={}
-ID=4
-NUM=0
+from playsound import playsound
 
-def sound(a):
-    playsound(a)
+from resources.LeapSDK.v53_python39 import Leap
+
+_JOINT_POSITIONS = {}  # contains the data which will be saved to a json file every 10s
+ID = 4  # participant's id
+NUM = 0  # index of the json file
 
 
-def task():
+def save_joint_data():
+    """
+    Dumps the data in _JOINT_POSITIONS to a json file, then does the same in 10s
+    """
     global _JOINT_POSITIONS
     global NUM
 
-    Timer(10.0, task).start()
+    Timer(10.0, save_joint_data).start()  # we want this to happen every 10s
     with open(f'joint_positions_id{ID}_num{NUM}.json', 'w') as fp:
         json.dump(_JOINT_POSITIONS.copy(), fp)
-    NUM+=1
+    NUM += 1
     _JOINT_POSITIONS.clear()
+
 
 class LeapRecord(Leap.Listener):
     finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
@@ -30,13 +33,11 @@ class LeapRecord(Leap.Listener):
 
     def __init__(self):
         super(LeapRecord, self).__init__()
-        self.tracking=False
+        self.tracking = False
         self.fps = 20
         self.processing = True
         self.t = None
         self.last_time = 0
-        
-
 
     def on_init(self, controller):
         self.t = Thread(target=self.process_frame, args=(self,))
@@ -54,9 +55,12 @@ class LeapRecord(Leap.Listener):
     def on_exit(self, controller):
         print("=====================")
         print("Exited")
-    
+
     def _check_frame(self, frame):
-        if not (len(frame.hands)==1 and len(frame.fingers)==5 and frame.hands[0].is_right):
+        """
+        Checks if the given frame contains a unique right hand
+        """
+        if not (len(frame.hands) == 1 and len(frame.fingers) == 5 and frame.hands[0].is_right):
             return False
         return True
 
@@ -64,28 +68,27 @@ class LeapRecord(Leap.Listener):
         # Get the most recent frame
         frame = controller.frame()
         if self._check_frame(frame):
-            if not self.tracking:
-                # playsound('detected.wav')
-                Thread(target=sound, args=("detected.wav",)).start()
-
-            self.tracking=True
-            hand_positions={}
+            if not self.tracking:  # play a sound if hand becomes detected
+                Thread(target=playsound, args=("detected.wav",)).start()
+            self.tracking = True
+            hand_positions = {}
             for hand in frame.hands:
-                joint_positions={'Wrist':(hand.arm.wrist_position[0],hand.arm.wrist_position[1],hand.arm.wrist_position[2])}
+                joint_positions = {
+                    'Wrist': (hand.arm.wrist_position[0], hand.arm.wrist_position[1], hand.arm.wrist_position[2])}
                 for finger in hand.fingers:
-                    finger_joints={}
+                    finger_joints = {}
                     for b in range(4):
-                        bone=finger.bone(b)
-                        finger_joints[self.bone_names[bone.type]]=(bone.prev_joint[0],bone.prev_joint[1],bone.prev_joint[2])
+                        bone = finger.bone(b)
+                        finger_joints[self.bone_names[bone.type]] = (
+                            bone.prev_joint[0], bone.prev_joint[1], bone.prev_joint[2])
                         # finger_joints[self.bone_names[bone.type]]=(bone.next_joint[0],bone.next_joint[1],bone.next_joint[2])
-                    joint_positions[self.finger_names[finger.type]]=finger_joints
-                hand_positions["Right" if hand.is_right else "Left"]=joint_positions
-            _JOINT_POSITIONS[int(time.time()*1000)]=hand_positions
+                    joint_positions[self.finger_names[finger.type]] = finger_joints
+                hand_positions["Right" if hand.is_right else "Left"] = joint_positions
+            _JOINT_POSITIONS[int(time.time() * 1000)] = hand_positions
         else:
-            if self.tracking or int(time.time()*1000)%900==0:
-                # playsound('lost.wav')
-                Thread(target=sound, args=("lost.wav",)).start()
-            self.tracking=False    
+            if self.tracking or int(time.time() * 1000) % 900 == 0:  # play a sound regularly if hand becomes undetected
+                Thread(target=playsound, args=("lost.wav",)).start()
+            self.tracking = False
 
     @staticmethod
     def process_frame(listener):
@@ -99,18 +102,18 @@ class LeapRecord(Leap.Listener):
     def exit_actions(self):
         print("exit")
 
+
 def main():
     # Create a listener and controller
     listener = LeapRecord()
     controller = Leap.Controller()
-    controller.set_policy_flags(Leap.Controller.POLICY_IMAGES)
-   
+
     # Have the listener receive events from the controller
     controller.add_listener(listener)
 
     # Keep this process running until Enter is pressed
     print("Listener added")
-    Timer(10.0, task).start()
+    Timer(10.0, save_joint_data).start()  # save joint data for the first time in 10s
     try:
         sys.stdin.readline()
     except KeyboardInterrupt:
@@ -121,9 +124,8 @@ def main():
         print("Listener removed")
         listener.exit()
     with open(f'joint_positions_id{ID}_num{NUM}.json', 'w') as fp:
-        json.dump(_JOINT_POSITIONS, fp)
+        json.dump(_JOINT_POSITIONS, fp)  # also save at the end to make sure we have all the data
 
-  
 
 if __name__ == "__main__":
     main()
